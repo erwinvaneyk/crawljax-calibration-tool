d0091e01060dd99d835e2e461e304d005ec224c1
Erwin van Eyk <Erwin_dso@hotmail.com>
2014-04-23 17:25:26 +0200

    Refactored ConnectionManager

--- src/main/java/distributed/ConnectionManager.java
+++ src/main/java/distributed/ConnectionManager.java
@@ -8,42 +8,43 @@ import java.util.logging.Logger;
 
 public class ConnectionManager {
 	public static String DEFAULT_SETTINGS_DIR = System.getProperty("user.dir") + "\\src\\main\\config";
+	public static String DRIVER = "com.mysql.jdbc.Driver";
 	
 	final Logger logger = Logger.getLogger(ConnectionManager.class.getName());
 	
 	private Connection connection;
 
 	private Properties settings;
+	private String url;
+	private String database;
+	private String username;
+	private String password;
 	
 	public ConnectionManager(String connectionDetails) throws IOException {
-		setupConnection(connectionDetails);
+		setup(connectionDetails);
 	}
 	
 	public ConnectionManager() throws IOException {
-		logger.warning("Using the default paths for the config-files.");
-		setupConnection(DEFAULT_SETTINGS_DIR + "/dist.ini");
+		logger.warning("ConnectionManager uses the default paths for the config-files.");
+		setup(DEFAULT_SETTINGS_DIR + "/dist.ini");
 	}
 	
 	
-	public void setupConnection(String connectionDetailsPath) throws IOException {
+	private void setup(String connectionDetailsPath) throws IOException {
 		settings = new Properties();
+		FileInputStream input = new FileInputStream(connectionDetailsPath);
+		 
+		// load a properties file
+		settings.load(input);
+		url = settings.getProperty("url");
+		database = settings.getProperty("database");
+		username = settings.getProperty("username");
+		password = settings.getProperty("password");
+		
+		
+		// Setup args
 		try {
-			FileInputStream input = new FileInputStream(connectionDetailsPath);
-			 
-			// load a properties file
-			settings.load(input);
-			
-			// Setup args
-			String driver = "com.mysql.jdbc.Driver";
-			Class.forName(driver).newInstance();
-			String url = settings.getProperty("url");
-			String database = settings.getProperty("database");
-			String username = settings.getProperty("username");
-			String password = settings.getProperty("password");
-			
-			connection = DriverManager.getConnection(url + database,username,password);
-			logger.info("Connection established with: " + url + database);
-			
+			Class.forName(DRIVER).newInstance();
 		} catch (InstantiationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -53,10 +54,18 @@ public class ConnectionManager {
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		logger.info("Connection settings loaded. User: " + username);
+	}
+	
+	public void startConnection() {
+		try {
+			connection = DriverManager.getConnection(url + database,username,password);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		logger.info("Connection established with: " + url + database);
 	}
 	
 	public void closeConnection() {
@@ -65,6 +74,7 @@ public class ConnectionManager {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		logger.info("Connection with: " + url + database + " closed.");
 	}
 
 	public Connection getConnection() {
