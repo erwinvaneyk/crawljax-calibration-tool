6778b43489e8fc11ca4db20ce62ac2b0a9037d98
Erwin van Eyk <Erwin_dso@hotmail.com>
2014-04-25 13:08:12 +0200

    Cleanup SuiteManager

--- src/main/java/SuiteManager.java
+++ src/main/java/SuiteManager.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -21,6 +22,10 @@ import java.util.logging.*;
 
 import com.crawljax.cli.JarRunner;
 
+/**
+ * SuiteManager is responsible for running the actual crawler. Therefore it 
+ * deals with the arguments needed by the CrawlJax CLI and websites to be crawled.
+ */
 public class SuiteManager {
 
 	final Logger logger = Logger.getLogger(SuiteManager.class.getName());
@@ -31,21 +36,37 @@ public class SuiteManager {
 	static final String INI_SECTION_COMMON = "common";
 	static final String ARG_WEBSITE = "website";
 	static final String ARG_OUTPUTDIR = "outputdir";
+	
 	private Queue<String> websiteQueue = new PriorityQueue<String>();
 
 	private Ini ini;
 
-
+	/**
+	 * Constructor for providing a non-default path to an ini.
+	 * @param iniPath path to a ini-file.
+	 * @throws IOException ini-file could not be found/accessed.
+	 */
 	public SuiteManager(String iniPath) throws IOException {
 		ini = new Ini(new FileReader(iniPath));
 	}
 
+	/**
+	 * Constructor for using the default ini.
+	 * @throws IOException the default ini could not be found.
+	 */
 	public SuiteManager() throws IOException {
 		logger.warning("Using the default paths for the config-file.");
 		ini = new Ini(new FileReader(DEFAULT_SETTINGS_DIR + DEFAULT_SETTINGS_INI));
 	}
 	
-	public HashMap<String,String> buildSettings(String website) throws URISyntaxException {
+	/**
+	 * Generates a map with settings extracted from the ini and website and outputdir-keys.
+	 * If defined custom settings of the website will be added. 
+	 * @param website the website for which the arguments are build.
+	 * @return map with arguments for the crawling of the website.
+	 * @throws URISyntaxException invalid website-url
+	 */
+	public Map<String,String> buildSettings(String website) throws URISyntaxException {
 		// Check URI
 		URI uri = new URI(website);
 
@@ -61,9 +82,15 @@ public class SuiteManager {
 	    // Setup vital arguments
 		args.put(ARG_WEBSITE, website);
 		args.put(ARG_OUTPUTDIR, generateOutputDir(website));
+		logger.info("Settings build for website: " + website);
 		return args;
 	}
 
+	/**
+	 * Reads a file containing website (1 website per line) and adds them to the queue.
+	 * @param websitesPath the path to the file containing websites.
+	 * @throws IOException the file could not be found.
+	 */
 	public void websitesFromFile(String websitesPath) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(websitesPath));
 		UrlValidator urlValidator = new UrlValidator();
@@ -76,12 +103,16 @@ public class SuiteManager {
 		logger.info("Website-queue loaded.");
 	}
 
+	/**
+	 * Crawl all websites in the queue.
+	 * @return the outputdirs of the crawled websites
+	 */
 	public ArrayList<String> crawlWebsites() {
 		String website = websiteQueue.poll();
 		ArrayList<String> outputdirs = new ArrayList<String>();
 		while(website != null) {
 			try {
-					HashMap<String,String> args = buildSettings(website);
+					Map<String,String> args = buildSettings(website);
 					runCrawler(args);
 					outputdirs.add(args.get(DEFAULT_OUTPUT_DIR));
 			} catch (URISyntaxException e) {
@@ -92,27 +123,29 @@ public class SuiteManager {
 		return outputdirs;
 	}
 
-	public String generateOutputDir(String website) {
+	/**
+	 * Generate a unique outputdir-name for a website
+	 * @param website the website needing a outputdir
+	 * @return unique name for the dir
+	 * @throws URISyntaxException website contains an invalid syntax
+	 */
+	public String generateOutputDir(String website) throws URISyntaxException {
 		Date date= new Date();
-		URI uri = null;
-		try {
-			uri = new URI(website);
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
+		URI uri = new URI(website);
 		Timestamp timestamp = new Timestamp(date.getTime());
 		return DEFAULT_OUTPUT_DIR + "/" + uri.getHost() + "-" + timestamp.getTime();
 	}
-
-	public void runCrawler(HashMap<String, String> args) {
-		logger.info("Starting crawler on: " + args.get(ARG_WEBSITE) + " outputting to: " + args.get(ARG_OUTPUTDIR) + ".");
-		
+	
+	/**
+	 * Run CrawlJax for a given set of args. Output can be found in args.get(ARG_OUTPUTDIR).
+	 * @param args arguments which need to be send to crawljax.
+	 */
+	public void runCrawler(Map<String, String> args) {		
 		// Convert args to valid args-line
 		String[] finargs = new String[(args.size() * 2 - 2)];
 		finargs[0] = args.remove(ARG_WEBSITE);
 		finargs[1] = args.remove(ARG_OUTPUTDIR);
 		int index = 2;
-		
 		for(String key : args.keySet()) {
 			finargs[index] = "-" + key;
 			finargs[index+1] = args.get(key);
@@ -123,6 +156,11 @@ public class SuiteManager {
 		logger.info("Finished crawling " + args.get(ARG_WEBSITE) + ".");
 	}
 	
+	/**
+	 * Add/replace additional setting to a existing settings-build.
+	 * @param args the argument-set to which the settings are added.
+	 * @param section the section (ini) that needs to be added.
+	 */
 	public void addSettings(HashMap<String,String> args, String section) {
 		try {
 			Section settings = ini.get(section);
@@ -135,11 +173,30 @@ public class SuiteManager {
 		}
 	}
 
+	/**
+	 * Get website-queue.
+	 * @return website-queue.
+	 */
 	public Queue<String> getWebsiteQueue() {
 		return websiteQueue;
 	}
 
+	/**
+	 * Set Website-queue
+	 * @param websiteQueue the new website-queue.
+	 */
 	public void setWebsiteQueue(Queue<String> websiteQueue) {
 		this.websiteQueue = websiteQueue;
 	}
+
+	/**
+	 * Set a new ini-object for setting-building. It requires a common-section to be present.
+	 * @param ini the new ini.
+	 */
+	public void setIni(Ini ini) {
+		assert ini.get(INI_SECTION_COMMON) != null;
+		this.ini = ini;
+	}
+	
+	
 }
