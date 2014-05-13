35332e8d8a4abf29d378a0a33226cd3715429f94
Erwin van Eyk <Erwin_dso@hotmail.com>
2014-05-13 15:24:31 +0200

    Refactored WorkTask to use ORMLite

--- src/main/java/distributed/workload/WorkTask.java
+++ src/main/java/distributed/workload/WorkTask.java
@@ -2,77 +2,34 @@ package main.java.distributed.workload;
 
 import java.net.URL;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 
 /**
  * A task for a worker to crawl a website
  */
-public class WorkTask {
-	private int id;
-	private URL url;
-	
-	public WorkTask(int id, URL url) {
-		assert url != null;
-		this.id = id;
-		this.url = url;
-	}
-	
-	/**
-	 * Set the id of the task.
-	 * @param id The identification for the task
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	/**
-	 * Set the url for the task.
-	 * @param url The website to crawl
-	 */
-	public void setUrl(URL url) {
-		assert url != null;
-		this.url = url;
-	}
-	
-	/**
-	 * @return The id of the task.
-	 */
-	public int getId() {
-		return id;
-	}
-	
-	/**
-	 * @return The url of this task
-	 */
-	public URL getUrl() {
-		return url;
-	}
+@DatabaseTable(tableName="workload")
+@RequiredArgsConstructor
+@Data public class WorkTask {
 
-	/**
-	 * @return the string representation of this task.
-	 */
-	public String toString() {
-
-		return "[" + id + ", " + url + "]";
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		WorkTask other = (WorkTask) obj;
-		if (id != other.id)
-			return false;
-		if (url == null) {
-			if (other.url != null)
-				return false;
-		} else if (!url.equals(other.url))
-			return false;
-		return true;
-	}
+	@DatabaseField(generatedId=true,canBeNull=false)
+	private final int id;
 	
+	@DatabaseField(canBeNull=false)
+	private final URL url;
+
+	@DatabaseField
+	private String crawler;
+
+	@DatabaseField
+	private boolean crawled;
 	
+	public WorkTask() {
+		id = 0;
+		url = null;
+	}
 }
