6c3be414453a7e14a19c168d123865e321f494d9
Erwin van Eyk <Erwin_dso@hotmail.com>
2014-05-19 10:40:35 +0200

    Added test cases for analysis-package

---
+++ src/test/main/analysis/TestAnalysisProcessorCmd.java
@@ -0,0 +1,44 @@
+package test.main.analysis;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import main.java.analysis.AnalysisException;
+import main.java.analysis.AnalysisProcessorCmd;
+import main.java.analysis.Analysis;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestAnalysisProcessorCmd {
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	}
+	
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
+	}
+	
+	@Test
+	public void testApply() throws AnalysisException {
+		Analysis analysisReport = mock(Analysis.class);
+		new AnalysisProcessorCmd().apply(analysisReport);
+		assertNotNull(outContent.toString());
+	}
+	
+	@Test
+	public void testApplyNull() throws AnalysisException {
+		new AnalysisProcessorCmd().apply(null);
+		assertNotNull(outContent.toString());
+	}
+}
---
+++ src/test/main/analysis/TestAnalysisProcessorFile.java
@@ -0,0 +1,54 @@
+package test.main.analysis;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import main.java.analysis.AnalysisProcessorFile;
+import main.java.analysis.Analysis;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestAnalysisProcessorFile {
+	
+	private AnalysisProcessorFile apf;
+	
+	private Analysis ap;
+	
+	private static File testDir = new File(System.getProperty("user.dir") + "/output/temp/");
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		testDir.mkdir();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		testDir.delete();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		apf = new AnalysisProcessorFile();
+		apf.setOutputDir(testDir);
+		ap = mock(Analysis.class);
+	}
+
+	@Test
+	public void testApply() {
+		when(ap.getTitle()).thenReturn("testApply");
+		apf.apply(ap);
+		assertTrue(apf.getOutput().exists());
+	}
+	
+	@Test
+	public void testApplyNull() {
+		apf.apply(null);
+		assertNull(apf.getOutput());
+	}
+}
