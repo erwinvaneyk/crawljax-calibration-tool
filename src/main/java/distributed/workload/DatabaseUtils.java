707ef5008c1fe15f9d6c029143c1379ea6240e2e
wilco van leeuwen <wilcovleeuwen@gmail.com>
2014-05-22 10:40:28 +0200

    Delete all results from db with a certain id

---
+++ src/main/java/distributed/workload/DatabaseUtils.java
@@ -0,0 +1,81 @@
+package main.java.distributed.workload;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import main.java.distributed.ConnectionManager;
+import main.java.distributed.IConnectionManager;
+
+public class DatabaseUtils {
+	
+	IConnectionManager con;
+	final Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	public DatabaseUtils(IConnectionManager con) {
+		this.con = con;
+	}
+	
+	/**
+	 * Delete all the results that are inserted in the database with the crawl of id.
+	 * @param id The id of the website, which results should be deleted
+	 * @return true if the results are correctly deleted
+	 */
+	public boolean deleteAllResultsById(int id) {
+		boolean result = false;
+		Connection connection = con.getConnection();
+		try {
+			String getWorkTaskId = "SELECT workTask_id FROM WebsiteResults WHERE id=?";
+			PreparedStatement statementWId = connection.prepareStatement(getWorkTaskId);
+			statementWId.setInt(1, id);
+			ResultSet resultWorkTask = statementWId.executeQuery();
+			int workTaskId = 0;
+			while (resultWorkTask.next()) {
+				workTaskId = resultWorkTask.getInt(1);
+				log.info("workTaskId={}", workTaskId);
+			}
+			log.info("Deleting the results of websiteId: " + id + ", workTaksId: " + workTaskId + "...");
+			
+			boolean deleteDomResult = this.deleteById("DomResults", "websiteResult_id", id, connection);
+			boolean deleteWebsiteResults = this.deleteById("WebsiteResults", "id", id, connection);
+			boolean deleteWorkTask = this.deleteById("workload", "id", workTaskId, connection);
+			
+			con.closeConnection();
+			if (deleteDomResult && deleteWebsiteResults && deleteWorkTask) {
+				log.info("Succesfull deleted the results of id={}", id);
+				result = true;
+			}
+		} catch (SQLException e) {
+			log.info("SQLException while deleting the results of id={}", id);
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	private boolean deleteById(String table, String column, int id, Connection connection) throws SQLException {
+		boolean succes = false;
+		
+		String sql = "DELETE FROM " + table + " WHERE " + column + " = ?";
+		PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		statement.setInt(1, id);
+		int deleteDom = statement.executeUpdate();
+		if (deleteDom > 0) {
+			succes = true;
+		} else {
+			log.info(""+deleteDom);;
+			log.warn("The {} of id={} can not be deleted.", table, id);
+		}
+		return succes;
+	}
+	
+	public static void main(String[] args) {
+		DatabaseUtils test = new DatabaseUtils(new ConnectionManager());
+		test.deleteAllResultsById(87);
+	}
+}
