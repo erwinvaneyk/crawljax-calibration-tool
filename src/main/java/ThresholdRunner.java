64e3321fb948e79a5ae9010a40ad80e6b73b74f8
Erwin van Eyk <Erwin_dso@hotmail.com>
2014-05-26 14:33:22 +0200

    Refactored ThresholdRunner

--- src/main/java/ThresholdRunner.java
+++ src/main/java/ThresholdRunner.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import main.java.analysis.*;
@@ -13,27 +15,36 @@ public class ThresholdRunner {
 	private static int[] websiteIds = new int[]{1,2,15,48,51,53};
 
 	public static void main(String[] args) {
+		ThresholdRunner tr = new ThresholdRunner();
+		List<Analysis> results = tr.analyseThresholds(1, 1, websiteIds);
+		for(Analysis analysis : results) {
+			new AnalysisProcessorCsv(filename).apply(analysis);
+		}
+	}
+	
+	public List<Analysis> analyseThresholds(int from, int to, int[] websiteids) {
+		assert from <= to;
+		assert websiteids.length > 0;
 		ConfigurationDAO config = new ConfigurationDAO(new ConnectionManager());
 		Map<String, String> defaultSettings = config.getConfiguration("common");
 		// Build factory
 		AnalysisFactory factory = new AnalysisFactory();
 		factory.addMetric(new SpeedMetric());
 		factory.addMetric(new StateAnalysisMetric());
+		List<Analysis> results = new ArrayList<Analysis>();
 		
-		for(int i = 1; i <= 5; i++) {
+		for(int i = from; i <= to; i++) {
 			try {
 				// update setting
 				config.updateConfiguration("common", "threshold", String.valueOf(i), 6);
 				// run crawler
-				Analysis analysis = factory.getAnalysis("threshold-" + i,websiteIds);
-				// Generate report
-				new AnalysisProcessorCsv(filename).apply(analysis);
+				results.add(factory.getAnalysis("threshold-" + i,websiteIds));
 			} catch (AnalysisException e) {
 				e.printStackTrace();
 			}
 		}
 		config.updateConfiguration("common", "threshold", defaultSettings.get("threshold"), 6);
 		System.out.println("Finished!");
+		return results;
 	}
-
 }
